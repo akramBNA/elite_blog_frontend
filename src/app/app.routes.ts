0aab57e5e552c140b25522f5d0d2747eff011511
Akram <akrambenaoun1993@gmail.com>
2025-08-11 23:28:45 +0100

    setup some routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../components/login/login.component';
+import { SignupComponent } from '../components/signup/signup.component';
+import { MainPageComponent } from '../components/main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'sign-up', component: SignupComponent},
+    { path: 'main-page', component: MainPageComponent}
+];
