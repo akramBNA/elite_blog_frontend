2fb2f802e9b9bc0efa1ad5d7a89d26584db86418
Akram <akrambenaoun1993@gmail.com>
2025-08-12 17:15:54 +0100

    create a roles services

---
+++ src/services/roles.services.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environments';
+
+interface RefreshResponse {
+  success: boolean;
+  accessToken?: string;
+  message?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class RolesService {
+  constructor(private http: HttpClient) {}
+
+  readonly base_url = environment.backendURL;
+
+  getAllRoles(): Observable<any> {
+    return this.http.get<any>(`${this.base_url}/roles/getAllRoles`);
+  }
+}
