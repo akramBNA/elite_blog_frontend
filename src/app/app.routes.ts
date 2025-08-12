99b68497e38bcd2d364c1b6982080899fc4a2e05
Akram <akrambenaoun1993@gmail.com>
2025-08-12 14:00:16 +0100

    create a users list route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SignupComponent } from '../components/signup/signup.component';
 import { MainPageComponent } from '../components/main-page/main-page.component';
 import { AccessDeniedComponent } from '../components/access-denied/access-denied.component';
 import { AuthGuard } from '../guards/auth.guard';
+import { UsersListComponent } from '../components/users-list/users-list.component';
 
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'access-denied', component: AccessDeniedComponent },
     { path: 'sign-up', component: SignupComponent},
-    { path: 'main-page', component: MainPageComponent, canActivate: [AuthGuard]}
+    { path: 'main-page', component: MainPageComponent, canActivate: [AuthGuard]},
+    { path: 'users-list', component: UsersListComponent, canActivate: [AuthGuard]},
 ];
