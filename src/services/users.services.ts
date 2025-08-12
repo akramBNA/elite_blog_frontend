031d8d383bacf8d01c546eba5356643a5db194f2
Akram <akrambenaoun1993@gmail.com>
2025-08-12 11:49:04 +0100

    create a refreshToken service

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -3,11 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environments';
 
+interface RefreshResponse {
+  success: boolean;
+  accessToken?: string;
+  message?: string;
+}
+
 @Injectable({ providedIn: 'root' })
 export class UsersService {
   constructor(private http: HttpClient) {}
 
-  readonly base_url = environment.backendURL;    
+  
+  readonly base_url = environment.backendURL;
+  
 
     login(user_data:any): Observable<any> {
       return this.http.post<any>(`${this.base_url}/users/login`, user_data);
@@ -20,6 +28,10 @@ export class UsersService {
     logout(refreshToken: string): Observable<any> {
       return this.http.post<any>(`${this.base_url}/users/logout`,{ refreshToken },
         { headers: { Authorization: `Bearer ${localStorage.getItem('accessToken') || ''}`} });
+    };
+
+    refreshToken(refreshToken:string): Observable<RefreshResponse> {
+      return this.http.post<RefreshResponse>(`${this.base_url}/users/refreshToken`, {refreshToken});
     };
 
 }
