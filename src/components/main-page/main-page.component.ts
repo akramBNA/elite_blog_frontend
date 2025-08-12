a273db6cd0833a300a3ce0410d9f4074e55deaca
Akram <akrambenaoun1993@gmail.com>
2025-08-12 10:49:40 +0100

    create a main-page component

--- src/components/main-page/main-page.component.ts
+++ src/components/main-page/main-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent {
+  isMenuOpen = false;
+  currentYear = new Date().getFullYear();
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
